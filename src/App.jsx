907d1e12cb7c67524a2722b538556508044919f7
Dipa-44 <dipaghosh0902@gmail.com>
2025-02-19 19:15:02 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className=' bg-amber-400'>
+        <p>lorevdsmvkevke</p>
+      </div>
+    </>
+  )
+}
+
+export default App
